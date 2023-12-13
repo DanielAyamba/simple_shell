12b39ce42d5961d9b994509120b8b18e3978af2c
bbaaah <abaah1995@gmail.com>
2023-12-13 10:20:03 -0800

    update

--- authors.c
+++ authors.c
@@ -1,6 +1,6 @@
-# file @generate-authors.sh. DO NOT EDIT.
+# file @generate - authors.sh. DO NOT EDIT.
 # This file lists all contributors to the repository.
-# See hack/generate-authors.sh to make modifications.
+# See hack / generate - authors.sh to make modifications.
 
 Daniel Ayamba Adamu <danielayamba05@gmail.com>
 Bernard Baah <abaah1995@gmail.com>
